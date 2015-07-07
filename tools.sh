d7d21528145e380f83b7c148b66f6145fc69a34c
Edmond Leung <leung.edmond@gmail.com>
2015-07-07 05:41:28 +0200

    Removed iterm2 preferences

--- tools.sh
+++ tools.sh
@@ -3,6 +3,3 @@ xcode-select --install
 
 # Install Homebrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-# Copy over iTerm 2 preferences
-cp init/com.googlecode.iterm2.plist ~/Library/Preferences/

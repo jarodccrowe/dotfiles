40c4fd7b23755d13b0087fbdd797740a20b2a246
Edmond Leung <leung.edmond@gmail.com>
2015-07-07 15:47:50 +1000

    Installing rvm as part of tools.sh

--- tools.sh
+++ tools.sh
@@ -3,3 +3,7 @@ xcode-select --install
 
 # Install Homebrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install RVM
+
+\curl -sSL https://get.rvm.io | bash -s stable --ruby
